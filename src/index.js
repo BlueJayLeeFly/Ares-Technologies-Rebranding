1ace7e8aafd336667316c03beb9273a57a63c9f3
Jay Lee <slee9111@conestogac.on.ca>
2022-06-26 14:37:16 -0400

    add img for header and menu set up

---
+++ src/index.js
@@ -0,0 +1,31 @@
+// DOM
+const menuButton = document.getElementById('menu-btn');
+
+// Boolean
+let isMenuOpen = false;
+
+let menuAnimation = gsap.fromTo(
+	'nav',
+	{ opacity: 0 },
+	{ duration: 0.7, ease: 'power1', display: 'flex', opacity: 1 }
+);
+menuAnimation.pause();
+
+// Menu button event handling
+menuButton.addEventListener('click', () => {
+	if (!isMenuOpen) {
+		menuAnimation.play();
+		gsap.fromTo(
+			'nav ul li',
+			{
+				x: 'random(-300, 300)',
+				y: 'random(-300, 300)',
+			},
+			{ duration: 0.7, ease: 'power1', x: 0, y: 0 }
+		);
+		isMenuOpen = !isMenuOpen;
+	} else {
+		menuAnimation.reverse();
+		isMenuOpen = !isMenuOpen;
+	}
+});
